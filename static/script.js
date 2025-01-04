d7318d193187448c0ec8521507b6e500edab0a42
Stanislav <97356799+sm0thh@users.noreply.github.com>
2025-01-04 23:12:32 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,12 @@
+// Получение данных о пользователе
+const tg = window.Telegram.WebApp;
+
+// Отображение имени пользователя
+tg.ready();
+document.getElementById('username').innerText = `Привет, ${tg.initDataUnsafe.user.first_name}!`;
+
+// Функция для закрытия мини-приложения
+function closeWebApp() {
+    tg.close();
+}
 
